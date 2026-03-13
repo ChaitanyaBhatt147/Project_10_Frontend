6e2043bf27fdc0433fd33deb3ef95acafe5f8572
ChaitanyaBhatt147 <bhattchaitanya43@gmail.com>
2026-03-13 16:45:39 +0530

    Add Angular ORSProject-10-UI scaffold
    Initial commit adding an Angular 15 application scaffold named ORSProject-10-UI. Includes project configuration (angular.json, tsconfig files), package.json, source tree (src/ with app module, components and services), editorconfig, .gitignore, VS Code launch/tasks/extensions settings, and a README.

---
+++ ORSProject-10-UI/src/app/http-service.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpServiceService {
+  token = '';
+  form = {
+    message: '',
+    error: false
+  };
+
+  constructor(private router: Router, private httpClient: HttpClient) {}
+
+  get(endpoint: any, callback: any) {
+    return this.httpClient.get(endpoint).subscribe((data) => {
+      console.log('Data :: ' + data);
+      callback(data);
+
+    });
+  }
+
+  post(endpoint: any, bean: any, callback: any) {
+    return this.httpClient.post(endpoint, bean).subscribe((data) => {
+      console.log(data);
+      callback(data);
+
+    }, error => {
+      console.log('ORS Error--', error);
+    });
+  }
+}
