ae255e1a7f8c01d5a90b85dae62af1121c6ce07b
ChaitanyaBhatt147 <bhattchaitanya43@gmail.com>
2026-03-14 19:04:12 +0530

    Add BaseCtl, components, routing and navbar
    Introduce a BaseCtl (Base.component.ts) to centralize form logic and HTTP APIs. Add multiple feature components and templates (college, course, dashboard, marksheet, navbar, role, student, subject, user) and wire them into app-routing.module. Simplify app.component.html to render the navbar and router-outlet, and update AppModule to declare the new components, import FormsModule/RouterModule, and register HttpServiceService, EndpointServiceService and ServiceLocatorService providers. These changes scaffold the UI and common controller/service wiring for the app.

--- ORSProject-10-UI/src/app/app.module.ts
+++ ORSProject-10-UI/src/app/app.module.ts
@@ -1,21 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpServiceService } from './http-service.service';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RoleComponent } from './role/role.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule } from '@angular/router';
+import { EndpointServiceService } from './endpoint-service.service';
+import { ServiceLocatorService } from './service-locator.service';
+import { UserComponent } from './user/user.component';
+import { CollegeComponent } from './college/college.component';
+import { StudentComponent } from './student/student.component';
+import { MarksheetComponent } from './marksheet/marksheet.component';
+import { CourseComponent } from './course/course.component';
+import { SubjectComponent } from './subject/subject.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RoleComponent,
+    DashboardComponent,
+    UserComponent,
+    CollegeComponent,
+    StudentComponent,
+    MarksheetComponent,
+    CourseComponent,
+    SubjectComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule,
+    FormsModule
   ],
-  providers: [HttpServiceService],
+  providers: [HttpServiceService,
+    EndpointServiceService,
+    ServiceLocatorService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
